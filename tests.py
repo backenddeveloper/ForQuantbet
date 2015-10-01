c97282c2bf9a6faa2c12e16e87842bbd8f597881
Graham Turner <grahamturnernew@hotmail.com>
2015-10-01 21:56:59 +0100

    Failed our process response test.

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
 from socketmock import SocketMock
 from get import Get
+from processResponse import ProcessResponse
+from localmath import GCD
 
 class TestGet(unittest.TestCase):
 
@@ -12,6 +14,20 @@ class TestGet(unittest.TestCase):
 	self.assertEquals(reply , ["<html><div>" , "test" , "</div></html>"])
 	self.assertEquals(mock.dataSent , "GET /quiz HTTP/1.1\nHost: quantbet.com\n\n")
 	self.assertEquals(mock.closeCalled , 1)
+
+class TestProcessResponse(unittest.TestCase):
+
+    def test_finds_tags(self):
+	mock = "<html><div><strong>17</strong><strong>101</strong></div></html>"
+	test = ProcessResponse(mock)
+	numbers = test.getNumbers()
+	self.assertEquals(numbers , [17,101])
+
+    def test_finds_cookie(self):
+	mock = "\nHost:quantbet.com \nCookie:thisisa brokencookie string \nAccept:gzip"
+	test = ProcessResponse(mock)
+	cookie = test.getCookie()
+	self.assertEquals(cookie , "thisisa brokencookie string ")
 
 if __name__ == '__main__':
     unittest.main()
